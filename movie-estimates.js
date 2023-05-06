5f85e0a49e96a70698eddee777787e683d0728b7
Noah Patullo <macrobyte2@gmail.com>
2023-05-06 16:58:05 -0400

    fix 24 hour times

--- movie-estimates.js
+++ movie-estimates.js
@@ -12,6 +12,14 @@ let getDurationInMinutes = (runtime) => {
 
 let stringToDate = (str) => new Date(`January 1 2023 ${str}`)	//date part doesn't matter
 
+const to24HourTime = (str) => {
+	let date = stringToDate(str);
+	if(date.getHours() < 12){
+		date.setHours(date.getHours() +12)
+	}
+	return date;
+}
+
 let getMovieEndTimes = (showtimes) =>{
 	let durationInMinutes = getDurationInMinutes(showtimes[0]);
 	let endTimes = []
@@ -23,12 +31,12 @@ let getMovieEndTimes = (showtimes) =>{
 	return endTimes;
 }
 
-let showtimes1 = [1.33, '3:30',  '6:00',  '9:05']
-let showtimes2 = [1.52, '3:10',  '6:10',  '8:45']
+let showtimes1 = [2.59, '12:00',   '3:40',   '7:25']
+let showtimes2 = [1.32, '12:50',  '3:25',  '6:35',  '8:55']
 let endTimes1 = getMovieEndTimes(showtimes1)
 let endTimes2 = getMovieEndTimes(showtimes2)
-console.log('End times:', endTimes1)
-console.log('End times:', endTimes2)
+console.log('End times 1:', endTimes1)
+console.log('End times 2:', endTimes2)
 
 // console.log(getDurationInMinutes(1.22))
 // console.log(getDurationInMinutes(2.22))
@@ -39,18 +47,20 @@ console.log('End times:', endTimes2)
 // console.log(getDurationInMinutes('2:10'))
 
 let getSmallestDiff = (showtimes1, endTimes1, showtimes2, endTimes2) =>{
+	showtimes1 = showtimes1.map((s, i) => i > 0 ? to24HourTime(s) : s)
+	showtimes2 = showtimes2.map((s, i) => i > 0 ? to24HourTime(s) : s)
 	let gap1to2 = []
 	let gap2to1 = []
 	for(let i=0; i< endTimes1.length; i++){
 		// console.log(`showtimes2 ${stringToDate(showtimes2[i+2])} end ${endTimes1[i]}`)	//offset by 2 since showtimes has runtime & want NEXT showtime
 		// console.log( Math.floor((stringToDate(showtimes2[i+2]) - endTimes1[i] )/1000 )/60 )
-		gap1to2.push(Math.floor((stringToDate(showtimes2[i+2]) - endTimes1[i] )/1000 )/60)		//1000 for ms to seconds, then 60 for seconds to minutes
-		gap2to1.push(Math.floor((stringToDate(showtimes1[i+2]) - endTimes2[i] )/1000 )/60)
+		gap1to2.push(Math.floor((showtimes2[i+2] - endTimes1[i] )/1000 )/60)		//1000 for ms to seconds, then 60 for seconds to minutes
+		gap2to1.push(Math.floor((showtimes1[i+2] - endTimes2[i] )/1000 )/60)
 	}
-	let min1To2 = Math.min(...gap1to2.filter(x => !isNaN(x)));
-	let min2To1 = Math.min(...gap2to1.filter(x => !isNaN(x)));
-	console.log(gap1to2, `Min = ${min1To2}`);
-	console.log(gap2to1, `Min = ${min2To1}`);
+	let min1To2 = Math.min(...gap1to2.filter(x => !isNaN(x) && x > 0));
+	let min2To1 = Math.min(...gap2to1.filter(x => !isNaN(x) && x > 0));
+	console.log('gap1to2', gap1to2, `Min = ${min1To2}`);
+	console.log('gap2to1', gap2to1, `Min = ${min2To1}`);
 	if(min1To2 <= min2To1){
 		console.log(`See movie 1 first, smallest gap ${min1To2}`)
 	}
